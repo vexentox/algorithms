602fd8de106ea9c540ac110f3a8ab445f593d28e
vexento <72956938+vexentox@users.noreply.github.com>
2023-04-16 21:30:10 +0800

    Update delete_nth.py

--- delete_nth.py
+++ delete_nth.py
@@ -1,6 +1,11 @@
 import collections
-
-def my_delete_nth(array, n): 
+def my_delete_nth(array, n):
+    """
+    function：给定一个数组和一个数字 n ，创建一个新数组，其中每个数字至多 n 次而不重新排列。
+    array : 数组状
+    n : 每个数字至多重复 n 次
+    return : 每个数字至多 n 次而不重新排列的数组
+    """
     counts, j = collections.defaultdict(int), 0
     for x in array:
         if counts[x] < n:
